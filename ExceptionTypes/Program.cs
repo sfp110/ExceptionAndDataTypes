df7d368e36fb295643b761c8aadde34f29145ac1
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-07-29 11:29:26 +0100

    ExeptionType Exercises

---
+++ ExceptionTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
