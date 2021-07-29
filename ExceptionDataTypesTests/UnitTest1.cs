952145b9c1ab03943808b99f9f0b99a5535c305f
Sergio Pessegueiro <iFrancisco.sp@icloud.com>
2021-07-29 14:31:42 +0100

    Add a 2nd lamda expression

--- ExceptionDataTypesTests/UnitTest1.cs
+++ ExceptionDataTypesTests/UnitTest1.cs
@@ -13,15 +13,26 @@ namespace ExceptionDataTypesTests
 
         [TestCase(-34)]
         [TestCase(-1)]
-        [TestCase(1)]
         public void WhenMarkIsLessThanZero_Grade_ThrowsAnArgumentOutOfRangeException(int grade)
         {
-            Assert.That(() => Program.Grade(grade), Throws.TypeOf<ArgumentOutOfRangeException>());
+            Assert.That(() => Program.Grade(grade), Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("Mark can't be : " + grade));
             // Do this first - () => Program.Grade(grade) 
 
             // and then compary it to Throws.TypeOf<ArgumentOutOfRangeException>()
-        
+            // Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("Mark can't be : " + grade)
+            // is another example of an expression to compare it to. It's more specific 
+
             // () => IS A LAMDA EXPRESSION  
         }
+
+        [TestCase(101)]
+        [TestCase(122)]
+        public void WhenMarkIsGreaterThan100_Grade_ThrowsAnArgumentOutOfRangeException(int grade)
+        {
+            Assert.That(() => Program.Grade(grade), Throws.TypeOf<ArgumentOutOfRangeException>().With.Message.Contains("Mark can't be : " + grade));
+            
+        }
+
+
     }
 }
